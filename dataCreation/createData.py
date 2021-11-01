06505aac0d3eeab68e82911d2f0054121d7730d5
Vasugi14 <vasugi.ganesh@hotmail.com>
2021-11-01 14:55:59 -0400

    Update createData.py
    added countries and datetime

--- dataCreation/createData.py
+++ dataCreation/createData.py
@@ -1,6 +1,10 @@
 import csv
 import pandas as pd
 import numpy as np
+import datetime
+
+now = datetime.datetime.now()
+countries = ["Canada", "USA", "Sweden"]
 
 class User:
 	def __init__(self, ID, IP):
@@ -29,8 +33,17 @@ for i in range (len(userID)):
 senders = np.random.choice(users, 1000)
 receivers = np.random.choice(users, 1000)
 byteTransfer = rng.integers(low=0, high=1000, size=1000)
+dest = np.random.choice(countries, 1000, p=[0.49, 0.49, 0.02])
 
-headers = ["Sender ID", "Sender IP", "Receiver ID", "Receiver IP", "Byte Transfer Amount"]
+times = []
+times.append(now)
+nxt = now + datetime.timedelta(0,13)
+times.append(nxt)
+for i in range (1000):
+    nxt = nxt + datetime.timedelta(0,13)
+    times.append(nxt)
+
+headers = ["Sender ID", "Sender IP", "Receiver ID", "Receiver IP", "Time", "Destination", "Byte Transfer Amount"]
 
 with open("tester.csv", "w") as f:
     writer = csv.writer(f)
@@ -42,6 +55,8 @@ for i in range (1000):
     st.append(senders[i].IP)
     st.append(receivers[i].ID)
     st.append(receivers[i].IP)
+    st.append(times[i])
+    st.append(dest[i])
     st.append(byteTransfer[i])
 
     with open("tester.csv", "a") as f:
@@ -49,5 +64,8 @@ for i in range (1000):
         writer.writerow(st)
 
 print("All finished")
+
+#role/level of access->different tables/active directories for data -> also a separate file
+#softwares allowed in the system-> also a separate file
 
 # for ip address random creation: https://stackoverflow.com/questions/21014618/python-randomly-generated-ip-address-as-string
