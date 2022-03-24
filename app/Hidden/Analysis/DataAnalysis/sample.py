02f9f39006135a30a32ce70235935c3591700e91
SamaMMahmoud <sama.mhmd9@gmail.com>
2022-03-24 15:07:46 -0400

    old push

--- app/Hidden/Analysis/DataAnalysis/sample.py
+++ app/Hidden/Analysis/DataAnalysis/sample.py
@@ -7,7 +7,7 @@ import json
 #QUERY BACKEND
 def getBackend():
     raq = json.loads("raqResults.json")
-    backend = file["backend"]
+    backend = raq["backend"]
     if backend == "CSV":
         import CSV as back
     elif backend == "MSSQL":
@@ -21,16 +21,15 @@ def getBackend():
     else:
         import PostgreSQL as back
     
-
 getBackend()
+
 data1 = back.readColumn("Sender IP", False)
 data2 = back.readColumn("Receiver IP", False)
-merged = back.mergeData(data1, data2)
-unique = back.makeUnique(merged)
+mergedData = back.mergeData(data1, data2)
+uniqueData = mergedData.drop_duplicates()
 
 # We can use this to create our list of nodes
-nodes = [{"id": str(unique_set[0]), "name": unique_set[0]} for unique_set in unique.values]
-
+nodes = [{"id": str(unique_set[0]), "name": unique_set[0]} for unique_set in uniqueData.values]
 
 ## Creating Links
 Send_to_receive = network_data[["srcip", "dstip"]].drop_duplicates()
@@ -38,11 +37,10 @@ Send_to_receive = network_data[["srcip", "dstip"]].drop_duplicates()
 # Create a list of established connections
 links = [{"source":str(unique_set[0]), "target": str(unique_set[1])} for unique_set in Send_to_receive.values]
 
-
 # Create the final data structure for a network!
 data = {"nodes":nodes, "links": links}
 
 with open('./app/Interface/MainApplication/network_data.json', 'w') as outfile:
     json.dump(data, outfile)
 
-requests.post("http://localhost:8000/network_data.json")
+requests.post("http://localhost:8000/network_data.json")  
